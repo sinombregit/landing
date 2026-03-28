61dc792525d2bd03d8b0a83e950cf0b79ed097d8
sinombre <renovazul@gmail.com>
2026-03-27 23:28:32 -0600

    Install AngularFire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,17 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "adminpro-7e194", appId: "1:179320935653:web:e218db25bdb6b33d0db00c", storageBucket: "adminpro-7e194.firebasestorage.app", apiKey: "AIzaSyCExmGBOeSk354Bi5rbiRRnKLpcvQguF14", authDomain: "adminpro-7e194.firebaseapp.com", messagingSenderId: "179320935653", measurementId: "G-RZ5PDQEH4X", projectNumber: "179320935653", version: "2" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ]
 };
